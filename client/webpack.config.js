e2883c92a6855f20088cbf8acf955e787617406d
chilejay7 <codyburk7@yahoo.com>
2024-01-12 20:27:09 -0700

    service worker debugged and running successfully

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const { GenerateSW }= require('workbox-wepack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
