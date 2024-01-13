9dc08b532856d90456717a7284fdee5429d012bb
chilejay7 <codyburk7@yahoo.com>
2024-01-13 14:04:22 -0700

    putDb function created in database.js to write notes to indexDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("Updating the database with your notes.");
+
+  // This statement opens the connection with the jate database defined beginning on line 3.
+  const jateDb = await openDB('jate', 1);
+
+  // Read and write permissions are being given to the transaction to allow new data to be written.
+  const transact = jateDb.transaction('jate', 'readwrite');
+
+  // The object store needs to be opened to allow the transaction.
+  const store = transact.objectStore('jate');
+
+  // The add method is being used to write the content passed to the application into the database.
+  const request = store.add({ note: content });
+
+  // We await the successful addition of the data to the local database.
+  const result = await request;
+
+  console.log('The data has been written to the jateDb', result);
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
