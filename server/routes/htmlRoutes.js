b18a63a95febfb03b8b51edd394a54e1a4c94477
chilejay7 <codyburk7@yahoo.com>
2024-01-13 13:50:13 -0700

    notes added

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+// No additional routes are required since the appliction only needs to server the index.html.
+// The database is run though  the browser using indexDB and won't be making additional requests to the server.
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// This is a static route set to serve the content of the bundled webpack files in the dist directory.
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
