676682252a1d0d081f9d7ef773e7877aabdf0531
chilejay7 <codyburk7@yahoo.com>
2024-01-12 20:44:06 -0700

    babel loader added to application

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -71,7 +71,18 @@ module.exports = () => {
             // keep original filenames and copy images to `dist/img/`
             filename: 'assets/icons/logo.png', 
           },
-        }
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
