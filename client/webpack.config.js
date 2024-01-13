da01f39b8c8452484d170411017001af5f6deb4f
chilejay7 <codyburk7@yahoo.com>
2024-01-12 20:15:27 -0700

    minicss extract plugin added to app

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-wepack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -30,6 +32,8 @@ module.exports = () => {
         title: 'JATE'
       }),
 
+      new MiniCssExtractPlugin(),
+
       new WebpackPwaManifest({
         name: 'JATE',
         short_name: 'JATE',
@@ -56,7 +60,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader']
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
