f81f8d44726973811ccff33e10b61d5eeec446ac
chilejay7 <codyburk7@yahoo.com>
2024-01-14 14:50:46 -0700

    install properties added to manifest and now works

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,47 +1,47 @@
 const btnInstall = document.getElementById('buttonInstall');
-// let installPrompt = null;
+let installPrompt = null;
 
 // // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault();
     console.log(`Before Install Prompt Event: ${e}`);
-    // installPrompt = e;
-    // console.log(`***Before Install prompt is: ${installPrompt}***`)
-    // btnInstall.classList.toggle('hidden', false);
-    // return installPrompt;
-
-    window.deferredPrompt = e;
+    installPrompt = e;
+    console.log(`***Before Install prompt is: ${installPrompt}***`)
     btnInstall.classList.toggle('hidden', false);
+    return installPrompt;
+
+    // window.deferredPrompt = e;
+    // btnInstall.classList.toggle('hidden', false);
 });
 
 // // TODO: Implement a click event handler on the `butInstall` element
 btnInstall.addEventListener('click', async (e) => {
 
-    const installPrompt = window.deferredPrompt;
-    console.log(`Install prompt is: ${window.deferredPrompt}`)
+    // const installPrompt = window.deferredPrompt;
+    // console.log(`Install prompt is: ${window.deferredPrompt}`)
 
-    // if (installPrompt) {
-    //     try {
-    //         const result = await installPrompt.prompt();
-    //         console.log(`Install prompt was: ${result.outcome}`)
-    //         installPrompt = null;
-    //         btnInstall.classList.toggle('hidden', true);
-    //         // btnInstall.textContent = 'Installed';
-    //     } catch (error) {
-    //         console.error('Error during installation:', error);
-    //     }
-    // }
+    if (installPrompt) {
+        try {
+            const result = await installPrompt.prompt();
+            console.log(`Install prompt was: ${result.outcome}`)
+            installPrompt = null;
+            btnInstall.classList.toggle('hidden', true);
+            btnInstall.textContent = 'Installed';
+        } catch (error) {
+            console.error('Error during installation:', error);
+        }
+    }
 
-    if (!installPrompt) {
-        return;
-       }
+    // if (!installPrompt) {
+    //     return;
+    //    }
      
-      installPrompt.prompt();
+    //   installPrompt.prompt();
        
-       window.deferredPrompt = null;
+    //    window.deferredPrompt = null;
        
-       btnInstall.classList.toggle('hidden', true);
+    //    btnInstall.classList.toggle('hidden', true);
 
 });
 
