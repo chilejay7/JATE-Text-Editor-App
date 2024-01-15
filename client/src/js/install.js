1361587d54646723ca83096601943bddb8f2f217
chilejay7 <codyburk7@yahoo.com>
2024-01-15 12:23:16 -0700

    notes added

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,12 @@
 const btnInstall = document.getElementById('buttonInstall');
+
+// This variable is defined and initially set to null.  It will be used to capture the before install prompt event when the window loads the application.
 let installPrompt = null;
 
-// // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Logic for installing the PWA
+// When browser loads the application in the window it will detect if it can be installed.
+// This event is captured in the installPrompt variable and will be stored for the time when the Install button is clicked to prompt for installation.
+// The event's default of allowing the browser to immediately prompt for installation is prevented.
 window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault();
     console.log(`Before Install Prompt Event: ${e}`);
@@ -12,15 +16,14 @@ window.addEventListener('beforeinstallprompt', (e) => {
     return installPrompt;
 });
 
-// // TODO: Implement a click event handler on the `butInstall` element
+// When the install button is clicked the prompt captured previously in the event is presented to the end-user to confirm installation.
+// A try catch is used to handle any potential errors during installation.
 btnInstall.addEventListener('click', async (e) => {
 
     if (installPrompt) {
         try {
             const result = await installPrompt.prompt();
             console.log(`Install prompt was: ${result.outcome}`)
-            installPrompt = null;
-            // btnInstall.classList.toggle('hidden', true);
             btnInstall.textContent = 'Installed';
         } catch (error) {
             console.error('Error during installation:', error);
@@ -28,7 +31,8 @@ btnInstall.addEventListener('click', async (e) => {
     }
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Confirmation of the installation is written to the console.
+// The installPrompt variable is reset to null.
 window.addEventListener('appinstalled', (e) => {
     console.log('The application has successfully been installed on your Desktop!', 'appInstalled', e);
     installPrompt = null;
