a88633014f5175349fc89d7371a278684d5d8f48
chilejay7 <codyburk7@yahoo.com>
2024-01-15 17:05:54 -0700

    notes added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -32,14 +31,20 @@ module.exports = () => {
         swDest: './src-sw.js'
       }),
 
+      // This plugin includes the HTML file in the bundle and provides the path to the file as well as the favicon.
+      // The title of the application is set 
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE',
         favicon: './favicon.ico'
       }),
 
+      // This plugin extracts the CSS into separate files instead of bundling it with other JavaScript files.
+      // The rules are further defined below.
       new MiniCssExtractPlugin(),
 
+      // This plugin is required to generate the manifest for the application and provide metadata to the browser.
+      // The manifest file and its information can be found in the Application module of the dev tools in the browser.
       new WebpackPwaManifest({
         name: 'JATE',
         short_name: 'JATE',
