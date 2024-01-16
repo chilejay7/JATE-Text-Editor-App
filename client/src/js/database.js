d1f1001348107e872be57c6e832c16dcd4fbed8b
chilejay7 <codyburk7@yahoo.com>
2024-01-15 17:14:29 -0700

    notes added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,11 +12,11 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Function used to update content within the IndexedDB database.
 export const putDb = async (content) => {
   console.log("Updating the database with your notes.");
 
-  // This statement opens the connection with the jate database defined beginning on line 3.
+  // This statement opens the connection with the jate database defined beginning on line 3 and specifies the version number of the database.
   const jateDb = await openDB('jate', 1);
 
   // Read and write permissions are being given to the transaction to allow new data to be written.
@@ -35,7 +35,7 @@ export const putDb = async (content) => {
 
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Method defined for retrieving notes stored in the database.
 export const getDb = async () => {
 
   console.log('Retrieving notes from the database');
